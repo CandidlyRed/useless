b0b0448968bf73bdb2d69331a0668c3581d0ed66
LeoryR <59667106+LeoryR@users.noreply.github.com>
2020-01-14 11:25:27 -0600

    Add files via upload
    skrrt

---
+++ Video/main.js
@@ -0,0 +1,38 @@
+﻿function check() {
+
+    var Base = document.form.Base.value;
+    var Sub = document.form.Sub_Melody.value;
+    var Main = document.form.Main_Melody.value;
+
+
+
+
+    if (Base == "1") {
+
+    } 
+    if (Base == "2") {
+
+    }
+    if (Base == "3") {
+
+    } 
+
+    if (Sub == "1") {
+
+    }
+    if (Sub == "2") {
+
+    }
+    if (Sub == "3") {
+
+    }
+    if (Sub == "1") {
+
+    }
+    if (Sub == "2") {
+
+    }
+    if (Sub == "3") {
+
+    } 
+}
